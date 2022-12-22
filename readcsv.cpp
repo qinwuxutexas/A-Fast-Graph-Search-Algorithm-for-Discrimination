2824b4b22d1670ab92133afc88c0c890820b9b94
Qinwu Xu <78186650+qinwuxutexas@users.noreply.github.com>
2022-12-22 13:39:51 -0500

    Delete readcsv.cpp

--- readcsv.cpp
+++
@@ -1,2 +0,0 @@
-//bbx_file_path = '/net/argon/algo3/boundingboxes/coryver/bb_merlin_mvt2.csv./*new_coryver_31nov2020'
-//bbx_file_path = '/net/argon/algo3/boundingboxes/coryver/bb_merlin_mvt2.csv.new_coryver_31nov2020'*/
